3bd510eaa613e9c5c951b7e3653df3f6616d1ad2
David Selassie <selassid@gmail.com>
2017-10-26 22:57:15 -0700

    Turns out we can get permalinks by hacking query params.

--- RMLSwebAddons.user.js
+++ RMLSwebAddons.user.js
@@ -12,38 +12,33 @@
     'use strict';
 
     let baseUrl = window.location.href.split('?')[0] + '?';
-    let queryParams = new URLSearchParams(document.location.search.substring(1));
+    let origQueryParams = new URLSearchParams(document.location.search.substring(1));
 
     let reportItemHeaders = document.querySelectorAll('.reportItemHeader');
     for (let reportItemHeader of reportItemHeaders) {
-        let mlsn = reportItemHeader.innerText.split(' ')[1].trim();
-
-        // Setup anchors we can link to.
-        reportItemHeader.id = mlsn;
+        let mlid = reportItemHeader.innerText.split(' ')[1].trim();
 
         // Get address and make Google Maps links right after internal map links.
         let reportBox = reportItemHeader.nextElementSibling;
         let existingMapLink = reportBox.querySelector('.MAPLINK_ADDRESS_FULL:not(.GOOGLE_EXT)');
         let propertyAddress = existingMapLink.parentElement.firstChild.nodeValue;
-        let newLink = document.createElement('a');
-        newLink.className = 'MAPLINK_ADDRESS_FULL GOOGLE_EXT';
-        newLink.href = 'https://www.google.com/maps/search/?api=1&query=' + propertyAddress;
-        newLink.setAttribute('target', 'gmap');
-        newLink.innerHTML = '<span class="BOXLINK BOXICON_M np" title="Google Map">&nbsp;G&nbsp;</span>';
+        let gmapLink = document.createElement('a');
+        gmapLink.className = 'MAPLINK_ADDRESS_FULL GOOGLE_EXT';
+        let gmapLinkQueryParams = new URLSearchParams();
+        gmapLinkQueryParams.set("api", 1);
+        gmapLinkQueryParams.set("query", propertyAddress);
+        gmapLink.href = 'https://www.google.com/maps/search/?' + gmapLinkQueryParams.toString();
+        gmapLink.setAttribute('target', 'gmap');
+        gmapLink.innerHTML = '<span class="BOXLINK BOXICON_M np" title="Google Map">&nbsp;G&nbsp;</span>';
         // Insert before.
-        existingMapLink.parentNode.insertBefore(newLink, existingMapLink.nextSibling);
+        existingMapLink.parentNode.insertBefore(gmapLink, existingMapLink.nextSibling);
 
         // Update header text to be permalink with address.
         let reportItemHeaderText = reportItemHeader.querySelector('b');
-        let linkQueryParams = new URLSearchParams(queryParams);
-        linkQueryParams.set("mlsn", mlsn);
-        let href = baseUrl + linkQueryParams.toString();
-        reportItemHeaderText.innerHTML = 'MLS#: <a href="' + href + '">' + mlsn + '</a> - ' + propertyAddress;
-    }
-
-    // Scroll to selected entry. Can't use anchor # because server parses it and hangs...
-    let mlsn = queryParams.get("mlsn");
-    if (mlsn) {
-        document.getElementById(mlsn).scrollIntoView();
+        let permalinkQueryParams = new URLSearchParams(origQueryParams);
+        permalinkQueryParams.set("DMD", 1);
+        permalinkQueryParams.set("MLID_ARRAY", ["", mlid].join(","));
+        let permalinkHref = baseUrl + permalinkQueryParams.toString();
+        reportItemHeaderText.innerHTML = 'MLS#: <a href="' + permalinkHref + '" target="_blank">' + mlid + '</a> - ' + propertyAddress;
     }
 })();
